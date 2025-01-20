f2d34023bae5b4c3c7697c80858b30501ddecaf6
mahutt <tommy.mahut@gmail.com>
2025-01-19 21:38:06 -0500

    Introduce failing test

--- client/__tests__/ThemedText-test.tsx
+++ client/__tests__/ThemedText-test.tsx
@@ -6,5 +6,6 @@ describe('<ThemedText />', () => {
     const tree = render(<ThemedText>Some text</ThemedText>).toJSON();
 
     expect(tree).toMatchSnapshot();
+    expect(true).toBe(false);
   });
 });
