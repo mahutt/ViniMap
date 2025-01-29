689505e0be8cb7188b1e44378bb0f804be11ed8b
Ambrose821 <mclaughlinambrose@gmail.com>
2025-01-29 17:58:04 -0500

    Testing protoype floor swithcing with Mapbox studio (DO NOT PULL REQUEST. THIS IS JUST FOR TESTING)

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, View } from 'react-native';
-import React from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+import React,{useState} from 'react';
 import Mapbox from '@rnmapbox/maps';
 import { SearchBar } from '@/components/SearchBar';
 
@@ -11,13 +11,31 @@ const DEFAULT_COORDINATES = {
   latitude: 45.4973,
 };
 
+
+
+
+
 export default function HomeScreen() {
   const mapRef = React.useRef<Mapbox.MapView | null>(null);
   const cameraRef = React.useRef<Mapbox.Camera | null>(null);
 
+  const[floorFilter,setFloorFilter] = useState(['==','level','hall9'])
+
+  const LAYER_ID = 'combined-geojson-dh6buu'
+
+  const updateFloorFilter = async (level: string)=>{
+    try{
+      setFloorFilter(['==', 'level', level])
+
+    }catch(error){
+      console.error('Error updating filter: ',error)
+    }
+  }
   return (
+    
+   
     <View style={styles.container}>
-      <Mapbox.MapView ref={mapRef} style={styles.map} styleURL={Mapbox.StyleURL.Street}>
+      <Mapbox.MapView ref={mapRef} style={styles.map} styleURL='mapbox://styles/ambrose821/cm6g7anat00kv01qmbxkze6i8'>
         <Mapbox.Camera
           ref={cameraRef}
           zoomLevel={15}
@@ -25,9 +43,30 @@ export default function HomeScreen() {
           animationMode="flyTo"
           animationDuration={2000}
         />
+
+
+          <Mapbox.ShapeSource
+          id="floorSource"
+          url="mapbox://ambrose821.bt7b663v"
+        >
+          <Mapbox.FillLayer
+            id="combined-geojson-dh6buu"
+            existing={true}
+            sourceID="floorSource"
+            filter={floorFilter} // Apply dynamic filtering
+            style={{
+              fillColor: 'hsl(0, 0%, 100%)', // White color as defined
+              fillOutlineColor: '#5c15d5',   // Purple outline color
+            }}
+          />
+        </Mapbox.ShapeSource>
       </Mapbox.MapView>
       <View style={styles.searchContainer}>
         <SearchBar onSearch={(query) => console.log(query)} />
+      </View>
+      <View>
+      <Button title="Ground Floor" onPress={() => updateFloorFilter('hall9')} />
+      <Button title="First Floor" onPress={() => updateFloorFilter('rewound')} />
       </View>
     </View>
   );
