db8b6a67ead2d2fcc4e43918adbcb1a9f7d363ef
mahutt <tommy.mahut@gmail.com>
2025-02-06 22:49:13 -0500

    Fix SonarQube issues

--- client/Services/RouteService.tsx
+++ client/Services/RouteService.tsx
@@ -176,7 +176,7 @@ class RouteService {
     return { coordinates: null, duration: null, distance: null };
   }
 
-  public static formatDuration = (seconds: number | null) => {
+  public static readonly formatDuration = (seconds: number | null) => {
     if (seconds === null) return 'Unavailable';
     const minutes = Math.round(seconds / 60);
     if (minutes >= 60) {
