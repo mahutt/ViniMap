e9c9fb1fc866f31f07d9c157789314fc6c6563c5
cmezzac <christophermezzacappa818@yahoo.ca>
2025-02-13 14:38:42 -0500

    fixed swiping problem

--- client/app/(tabs)/calendar.tsx
+++ client/app/(tabs)/calendar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useRef } from 'react';
 import {
   StyleSheet,
   Dimensions,
@@ -8,21 +8,29 @@ import {
   Text,
   TouchableOpacity,
 } from 'react-native';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import Swiper from 'react-native-swiper';
 import moment from 'moment';
+import Swiper from 'react-native-swiper';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 const { width } = Dimensions.get('window');
 
 export default function Example() {
+  const swiper = useRef();
+  const contentSwiper = useRef();
   const [week, setWeek] = useState(0);
+
   const [value, setValue] = useState(new Date());
 
-  const weeks = useMemo(() => {
+  /**
+   * Create an array of weekdays for previous, current, and next weeks.
+   */
+  const weeks = React.useMemo(() => {
     const start = moment().add(week, 'weeks').startOf('week');
+
     return [-1, 0, 1].map((adj) => {
       return Array.from({ length: 7 }).map((_, index) => {
         const date = moment(start).add(adj, 'week').add(index, 'day');
+
         return {
           weekday: date.format('ddd'),
           date: date.toDate(),
@@ -31,7 +39,10 @@ export default function Example() {
     });
   }, [week]);
 
-  const days = useMemo(() => {
+  /**
+   * Create an array of days for yesterday, today, and tomorrow.
+   */
+  const days = React.useMemo(() => {
     return [moment(value).subtract(1, 'day').toDate(), value, moment(value).add(1, 'day').toDate()];
   }, [value]);
 
@@ -45,17 +56,21 @@ export default function Example() {
         <View style={styles.picker}>
           <Swiper
             index={1}
+            ref={swiper}
             loop={false}
             showsPagination={false}
             onIndexChanged={(ind) => {
-              if (ind !== 1) {
-                setValue(
-                  moment(value)
-                    .add(ind - 1, 'week')
-                    .toDate()
-                );
-                setTimeout(() => setWeek(week + (ind - 1)), 10);
+              if (ind === 1) {
+                return;
               }
+
+              const index = ind - 1;
+              setValue(moment(value).add(index, 'week').toDate());
+
+              setTimeout(() => {
+                setWeek(week + index);
+                swiper.current.scrollTo(1, false);
+              }, 10);
             }}>
             {weeks.map((dates, index) => (
               <View style={styles.itemRow} key={index}>
@@ -66,7 +81,10 @@ export default function Example() {
                       <View
                         style={[
                           styles.item,
-                          isActive && { backgroundColor: '#111', borderColor: '#111' },
+                          isActive && {
+                            backgroundColor: '#111',
+                            borderColor: '#111',
+                          },
                         ]}>
                         <Text style={[styles.itemWeekday, isActive && { color: '#fff' }]}>
                           {item.weekday}
@@ -85,29 +103,38 @@ export default function Example() {
 
         <Swiper
           index={1}
+          ref={contentSwiper}
           loop={false}
           showsPagination={false}
           onIndexChanged={(ind) => {
-            if (ind !== 1) {
-              setTimeout(() => {
-                const nextValue = moment(value).add(ind - 1, 'days');
-                if (moment(value).week() !== nextValue.week()) {
-                  setWeek(moment(value).isBefore(nextValue) ? week + 1 : week - 1);
-                }
-                setValue(nextValue.toDate());
-              }, 10);
+            if (ind === 1) {
+              return;
             }
+
+            setTimeout(() => {
+              const nextValue = moment(value).add(ind - 1, 'days');
+
+              // Adjust week picker if needed
+              if (moment(value).week() !== nextValue.week()) {
+                setWeek(moment(value).isBefore(nextValue) ? week + 1 : week - 1);
+              }
+
+              setValue(nextValue.toDate());
+              contentSwiper.current.scrollTo(1, false);
+            }, 10);
           }}>
-          {days.map((day, index) => (
-            <View key={index} style={{ flex: 1, paddingHorizontal: 16, paddingVertical: 24 }}>
-              <Text style={styles.subtitle}>
-                {day.toLocaleDateString('en-US', { dateStyle: 'full' })}
-              </Text>
-              <View style={styles.placeholder}>
-                <View style={styles.placeholderInset} />
+          {days.map((day, index) => {
+            return (
+              <View key={index} style={{ flex: 1, paddingHorizontal: 16, paddingVertical: 24 }}>
+                <Text style={styles.subtitle}>
+                  {day.toLocaleDateString('en-US', { dateStyle: 'full' })}
+                </Text>
+                <View style={styles.placeholder}>
+                  <View style={styles.placeholderInset}>{/* Replace with your content */}</View>
+                </View>
               </View>
-            </View>
-          ))}
+            );
+          })}
         </Swiper>
       </View>
     </SafeAreaView>
@@ -145,6 +172,7 @@ const styles = StyleSheet.create({
     marginTop: 'auto',
     paddingHorizontal: 16,
   },
+  /** Item */
   item: {
     flex: 1,
     height: 50,
@@ -175,9 +203,14 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     color: '#111',
   },
+  /** Placeholder */
   placeholder: {
     flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
     height: 400,
+    marginTop: 0,
+    padding: 0,
     backgroundColor: 'transparent',
     marginBottom: '5%',
   },
@@ -187,7 +220,10 @@ const styles = StyleSheet.create({
     borderStyle: 'dashed',
     borderRadius: 9,
     flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
   },
+  /** Button */
   btn: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -195,11 +231,13 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     paddingVertical: 10,
     paddingHorizontal: 20,
+    borderWidth: 1,
     backgroundColor: '#007aff',
     borderColor: '#007aff',
   },
   btnText: {
     fontSize: 18,
+    lineHeight: 26,
     fontWeight: '600',
     color: '#fff',
   },
