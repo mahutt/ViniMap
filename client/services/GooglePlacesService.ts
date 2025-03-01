3ad0641f605e53bd20b15667b0c41246e3fb962b
annabelzecchel <zecchelannabel@gmail.com>
2025-02-28 23:46:27 -0500

    setup the Google places Service file

---
+++ client/services/GooglePlacesService.ts
@@ -0,0 +1,7 @@
+import { Coordinates } from '@/modules/map/MapContext';
+
+const GOOGLE_API_KEY = process.env.EXPO_PUBLIC_GOOGLEMAPS_API_KEY as string;
+
+class GooglePlacesService {}
+
+export default new GooglePlacesService();
