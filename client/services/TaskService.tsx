93d2b3b2527f71ed968af89985dd960079ca3e4d
cmezzac <christophermezzacappa818@yahoo.ca>
2025-03-20 10:49:32 -0400

    task service

--- client/services/TaskService.tsx
+++ client/services/TaskService.tsx
@@ -1,23 +1,37 @@
 import { getRoute } from '@/modules/map/MapService';
-import { Route, Task } from '@/modules/map/Types';
+import { Route, Task, Segment } from '@/modules/map/Types';
 
-class TaskService {
-  static async getOptimalRouteForPaths(listOfTasks: Task[]) {
-    if (listOfTasks.length < 2) {
-      throw new Error('At least two tasks are required to calculate a route.');
+export class TaskService {
+  static async getOptimalRouteForPaths(listOfTasks: Task[]): Promise<Route> {
+    if (listOfTasks.length <= 0) {
+      throw new Error('No tasks are selected');
     }
 
-    let fullRouteCoordinates: any[] = [];
+    let totalDistance = 0;
+    let totalDuration = 0;
+    let fullSegments: Segment[] = [];
 
     for (let i = 0; i < listOfTasks.length - 1; i++) {
       const startCoordinates = listOfTasks[i].location;
       const endCoordinates = listOfTasks[i + 1].location;
 
-      const route = await getRoute(startCoordinates, endCoordinates, 'walking');
+      try {
+        const route = await getRoute(startCoordinates, endCoordinates, 'walking');
 
-      console.log(route);
+        if (route?.segments?.length) {
+          totalDistance += route.distance;
+          totalDuration += route.duration;
+
+          fullSegments = [...fullSegments, ...route.segments];
+        }
+      } catch (error) {
+        console.error(`Error generating route from task ${i} to ${i + 1}:`, error);
+      }
     }
-
-    return fullRouteCoordinates;
+    return {
+      duration: totalDuration,
+      distance: totalDistance,
+      segments: fullSegments,
+    };
   }
 }
