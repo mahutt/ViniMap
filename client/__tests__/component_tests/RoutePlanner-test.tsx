4d8478f441d266abff5ef77f0d2288a96aa5da06
mahutt <tommy.mahut@gmail.com>
2025-03-22 23:44:24 -0400

    Fix broken test

--- client/__tests__/component_tests/RoutePlanner-test.tsx
+++ client/__tests__/component_tests/RoutePlanner-test.tsx
@@ -102,6 +102,7 @@ describe('RoutePlanner', () => {
     expect(mockLoadRouteFromCoordinates).toHaveBeenCalledWith(
       mockStartLocation,
       mockEndLocation,
+      'walking',
       'walking'
     );
   });
